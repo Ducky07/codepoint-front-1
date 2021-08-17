4a707de81bfdff734a07b1f8adbd59d5bd5205f9
Ville Säkkinen <sakkisen.ville@gmail.com>
2021-08-17 10:57:27 +0300

    initial PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const {i18n} = require('./next-i18next.config')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-module.exports = {
-  reactStrictMode: true,
+module.exports = withPWA({
+  pwa:{
+    dest: 'public',
+    runtimeCaching,
+
+  },
   images: {domains: ['avatars.githubusercontent.com']},
+  reactStrictMode: true,
   i18n,
-}
+})
